1521c8f580897f9a19263292d5d8b638d350077e
subbum <subbu@gmail.com>
2023-07-21 20:29:32 +0100

    echoing from terraform apply

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,17 +2,27 @@ def call() {
     pipeline {
         agent any
 
+        parameters{
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+        }
+        
+        options{
+            timestamps()
+            ansiColor('xterm')
+        }
+
         stages {
 
             stage('Init'){
                 steps {
-                    sh 'terraform init -backend-config=env-dev/state.tfvars'
+                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
                 }
             }
         
             stage('Apply'){
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    //sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    sh "echo"
                 }
             }
         }
