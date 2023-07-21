ad7fe831c311f86a858fa034a2ced031524890ee
subbum <subbu@gmail.com>
2023-07-21 16:27:31 +0100

    Created shared library of jenkins to make code dry to be used in other projects

---
+++ vars/terraform.groovy
@@ -0,0 +1,19 @@
+def call(){
+    pipeline{
+        agent any
+
+        stages{
+
+            stage('Init')
+                steps{
+                    sh 'terraform init -backend-config=env-dev/state.tfvars'
+                }
+            }
+            stage('Apply')
+                steps{
+                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                }
+            }
+        }
+    }
+}
