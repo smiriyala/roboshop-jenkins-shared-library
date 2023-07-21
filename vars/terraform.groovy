84aeed8a957910311f8b55baccaaf6bfbde1cd79
subbum <subbu@gmail.com>
2023-07-21 16:38:24 +0100

    groovy method udpated

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,12 +4,13 @@ def call(){
 
         stages{
 
-            stage('Init')
+            stage('Init'){
                 steps{
                     sh 'terraform init -backend-config=env-dev/state.tfvars'
                 }
             }
-            stage('Apply')
+        
+            stage('Apply'){
                 steps{
                     sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
                 }
