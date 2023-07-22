c7252fb39707b3da57349ba96db6017d4bf16387
subbum <subbu@gmail.com>
2023-07-22 05:56:53 +0100

    Action added to destory or apply for terrraform in jenkins execution

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,6 +4,7 @@ def call() {
 
         parameters{
             string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Environment?')
         }
         
         options{
@@ -22,6 +23,7 @@ def call() {
             stage('Apply'){
                 steps {
                     //sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    //sh 'terraform ${ACTION} -auto-approve -var-file=env-dev/main.tfvars'
                     sh "echo"
                 }
             }
