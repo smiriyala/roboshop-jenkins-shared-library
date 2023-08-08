b1c5c60a5782df10eaa931c332001c67f20058a1
subbum <subbu@gmail.com>
2023-08-08 14:33:51 +0100

    common groovy updated to Amazon EC Repo

--- vars/common.groovy
+++ vars/common.groovy
@@ -36,17 +36,23 @@ def codequality() {
 
 
 def prepareArtifacts() {
-    // created version file which goes inside build folder and automatically included in artifact
-    sh 'echo ${TAG_NAME} >VERSION'
+    /*
+        ### Below code commented in part of DOCKER RePO Implementation
+    */
+    // // created version file which goes inside build folder and automatically included in artifact
+    // sh 'echo ${TAG_NAME} >VERSION'
 
-    // here we are checking the app_lang variable  to check what to be included -x Jenkinsfile to exclude from zip
-    if (app_lang == "nodejs" || app_lang == "angular" || app_lang == "python" || app_lang == "golang")  {
-        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-    }
-    //shipping app is maven, using jar file instead of zipping all, schema added
-    if (app_lang == "maven")  {
-        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
-    }
+    // // here we are checking the app_lang variable  to check what to be included -x Jenkinsfile to exclude from zip
+    // if (app_lang == "nodejs" || app_lang == "angular" || app_lang == "python" || app_lang == "golang")  {
+    //     sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+    // }
+    // //shipping app is maven, using jar file instead of zipping all, schema added
+    // if (app_lang == "maven")  {
+    //     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+    // }
+
+    sh 'docker build -t 934235628328.dkr.ecr.us-east-1.amazonaws.com/${component}-${TAG_NAME} .'
+
 
 }
 
@@ -60,16 +66,23 @@ def artifactUpload() {
     //     sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://44.201.250.69:8081/repository/${component}/${component}-${TAG_NAME}.zip'
     // // }
 
+
+    /*
+        ### Below code commented in part of DOCKER RePO Implementation
+    */
     //NEW CODE TO PASSWORD MASKING PLUIGIN IN JENKINS.
-    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
-    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
-    wrap([$class: 'MaskPasswordsBuildWrapper',
-       varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
-        sh 'echo ${TAG_NAME} >VERSION'
-        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://44.203.41.15:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+    // env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    // env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    // wrap([$class: 'MaskPasswordsBuildWrapper',
+    //    varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+    //     sh 'echo ${TAG_NAME} >VERSION'
+    //     sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://44.203.41.15:8081/repository/${component}/${component}-${TAG_NAME}.zip'
 
-    }
+    // }
 
+    //New CODE;
+    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 934235628328.dkr.ecr.us-east-1.amazonaws.com'
+    sh 'docker push 934235628328.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
 }
 
 
